6b80bc6cb103b1bca73e3a410b22b1d0e0683371
Dwipayan Tripathy <dwi.tripathy@gmail.com>
2016-10-05 16:49:43 +0530

    updated on ignore files

--- Controllers/homeController.cs
+++ Controllers/homeController.cs
@@ -18,20 +18,10 @@ namespace LiveOdia.APi.Controllers
         }
 
         // GET: api/home/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
         // POST: api/home
         public bool Post(AdminModel value)
         {
             return dbutility.addNewCategory(value);
-        }
-
-        // PUT: api/home/5
-        public void Put(int id, [FromBody]string value)
-        {
         }
 
         // DELETE: api/home/5
